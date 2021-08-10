22d4661b35fe1470bbf1a0241654dae016041583
Takahiro Abe <hirotaka081630@gmail.com>
2021-08-10 13:52:15 +0900

    feat(35~36): forkについて

---
+++ 35.rb
@@ -0,0 +1,5 @@
+if fork
+	puts "entered the if block"
+else
+	puts "entered the else block"
+end
---
+++ 36.rb
@@ -0,0 +1,9 @@
+puts "parents process pid is #{Process.pid}"
+
+if fork
+	puts "entered the if block from #{Process.pid}"
+else
+	puts "entered the else block from #{Process.pid}"
+end
+
+puts fork
